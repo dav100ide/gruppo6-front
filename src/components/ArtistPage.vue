b9e4d171a812f4a4a80f169b64e4aeaeb8df1c4d
dav100ide <oreglio.dev@gmail.com>
2023-02-15 11:34:28 +0100

    artist => artists ho un array di datiSuCuiLavorare

--- src/components/ArtistPage.vue
+++ src/components/ArtistPage.vue
@@ -1,22 +1,48 @@
 <template>
+   <header>header in condivisione</header>
    <div class="container"></div>
+   <footer>footer in condivisione</footer>
 </template>
 
 <script>
 export default {
    data() {
       return {
-         artist: {
-            artist_nickname: 'Lil Uzi Vert',
-            introduction_text:
-               'Ciao sono Lil Uzi, lorem aspjdhaiop d opiwjapdja jpdjwpoajd pajpwdo jjd wp',
-            profile_photo: 'https://picsum.photos/200/300',
-            slug: 'lil-uzi-vert',
-            address: 'Via Cremona 52',
-            telephone: '335 5001874',
-            rating: 5,
-            reviews: ['review 1', 'review 2', 'review 3'],
-         },
+         artists: [
+            {
+               artist_nickname: 'Lil Uzi Vert',
+               introduction_text:
+                  'Ciao sono Lil Uzi, lorem aspjdhaiop d opiwjapdja jpdjwpoajd pajpwdo jjd wp',
+               profile_photo: 'https://picsum.photos/200/300',
+               slug: 'lil-uzi-vert',
+               address: 'Via Cremona 52',
+               telephone: '335 5001874',
+               rating: 5,
+               reviews: ['review 1', 'review 2', 'review 3'],
+            },
+            {
+               artist_nickname: 'Lil pu,',
+               introduction_text:
+                  'Ciao sono Lil pump, lorem aspjdhaiop d opiwjapdja jpdjwpoajd pajpwdo jjd wp',
+               profile_photo: 'https://picsum.photos/300/300',
+               slug: 'lil-pump',
+               address: 'Via Roma 52',
+               telephone: '335 555374',
+               rating: 0,
+               reviews: ['review 1', 'review 2', 'review 3'],
+            },
+            {
+               artist_nickname: 'Cordae',
+               introduction_text:
+                  'Ciao sono CORDAE, lorem aspjdhaiop d opiwjapdja jpdjwpoajd pajpwdo jjd wp',
+               profile_photo: 'https://picsum.photos/500/400',
+               slug: 'cordae',
+               address: 'Viale hotel 9',
+               telephone: '735 5001874',
+               rating: 2,
+               reviews: ['review 1', 'review 2', 'review 3'],
+            },
+         ],
       };
    },
 };
