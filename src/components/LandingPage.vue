59b0cd09ca98d2e639275878773874594451c9db
muac404 <muac404@gmail.com>
2023-02-15 13:32:29 +0100

    Layout landing page upgrade

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,14 +1,31 @@
+<!-- JAVASCRIPT -->
 <script>
 export default {};
 </script>
+<!-- /JAVASCRIPT -->
+
+<!-- parte grafica -->
 
 <template>
-  <h2>LANDING PAGE</h2>
+  <section class="landing-page">
+    <div>
+      <div>LANDING PAGE</div>
+    </div>
+    <div>
+      <div class="jumbotron hero">
+        <img src="img/landing-page.png" alt="jumbotron" />
+      </div>
+    </div>
+  </section>
 </template>
 
+<!-- /parte grafica -->
+
+<!-- scoped css -->
 <style lang="scss" scoped>
-h2 {
-  color: red;
-  text-align: center;
+.landing-page {
+  height: 100vh;
 }
 </style>
+
+<!-- scoped css -->
