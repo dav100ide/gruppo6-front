e2627191686d461b899869597c28fbc47c1912f3
jiemarabanzado <jiemar.abanzado@yahoo.it>
2023-02-15 10:03:10 +0100

    tolto intera ciao section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,19 @@
 <template>
    <main>
-      <CiaoSection />
+   
    </main>
 </template>
 
 <script>
-import CiaoSection from './MainSections/CiaoSection.vue';
+
 export default {
    components: {
-      CiaoSection,
+      
    },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+
+</style>
