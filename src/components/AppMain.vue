95cc7b57be51d664d2fc7d8642197037b347f921
dav100ide <oreglio.dev@gmail.com>
2023-02-21 12:58:39 +0100

    add main di prova con i routerLinks

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,35 @@
 <template>
-   <main>main</main>
+   <main>
+      <h3>-MAIN di prova-</h3>
+      <nav>
+         <RouterLink :to="{ name: 'home-page' }">HOMEPAGE</RouterLink>
+         <RouterLink :to="{ name: 'artist-page' }">artista page (PATH NON DINAMICO)</RouterLink>
+         <RouterLink :to="{ name: 'search-results-page' }">SearchResultsPage</RouterLink>
+      </nav>
+      <RouterView />
+   </main>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+   padding: 1rem;
+   border: 2px dashed #111;
+}
+nav {
+   color: white;
+   a {
+      display: inline-block;
+      margin: 1rem;
+      padding: 0.5rem;
+      border-radius: 15px;
+      background-color: blue;
+      &:hover {
+         text-decoration: underline;
+      }
+   }
+}
+</style>
