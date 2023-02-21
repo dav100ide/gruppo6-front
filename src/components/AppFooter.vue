3bf2989b29b090e7dbd095a623953df691d47ad8
dav100ide <oreglio.dev@gmail.com>
2023-02-21 13:47:08 +0100

    toglie logo footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,46 +1,41 @@
 <script></script>
 <template>
-  <section>
-    <footer class="bg-dark">
-      <!-- <h4 class="bg-dark">footer_Top</h4> -->
-      <ul class="d-flex justify-content-center">
-        <li class="px-2 footer-nav">Artist_Page</li>
-        <li class="px-2 footer-nav">Dashboard</li>
-        <li class="px-2 footer-nav">sponsor</li>
-      </ul>
+   <section>
+      <footer class="bg-dark">
+         <!-- <h4 class="bg-dark">footer_Top</h4> -->
+         <ul class="d-flex justify-content-center">
+            <li class="px-2 footer-nav">Artist_Page</li>
+            <li class="px-2 footer-nav">Dashboard</li>
+            <li class="px-2 footer-nav">sponsor</li>
+         </ul>
 
-      <!-- <h4 class="bg-dark">footer_Bottom</h4> -->
-      <ul class="d-flex justify-content-center">
-        <li class="px-4 footer-nav">about_us</li>
-        <li class="px-4 footer-nav">contact-us</li>
-        <li class="px-4 footer-nav">partnership</li>
-      </ul>
-      <div class="img-footer text-center">
-        <img
-          src="./IMG LOCALI/BOOLEAN 76 PROGETTO FINALE-Recuperato.png"
-          alt="logo"
-        />
-      </div>
-    </footer>
-  </section>
+         <!-- <h4 class="bg-dark">footer_Bottom</h4> -->
+         <ul class="d-flex justify-content-center">
+            <li class="px-4 footer-nav">about_us</li>
+            <li class="px-4 footer-nav">contact-us</li>
+            <li class="px-4 footer-nav">partnership</li>
+         </ul>
+         <div class="img-footer text-center">LOGO</div>
+      </footer>
+   </section>
 </template>
 
 <style lang="scss" scoped>
 footer {
-  color: white;
-  background-color: black;
-  height: 100vh;
+   color: white;
+   background-color: black;
+   height: 100vh;
 }
 footer li:hover {
-  color: white;
-  background-color: greenyellow;
-  cursor: pointer;
+   color: white;
+   background-color: greenyellow;
+   cursor: pointer;
 }
 .footer-nav {
-  list-style: none;
+   list-style: none;
 }
 
 .img-footer img {
-  width: 300px;
+   width: 300px;
 }
 </style>
