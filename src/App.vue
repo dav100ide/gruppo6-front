9f64d93654215137576c2ea752b5eed7eecf7099
dav100ide <oreglio.dev@gmail.com>
2023-02-21 11:10:06 +0100

    bug: prettier reformat push

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <script>
-import LandingPage from "./components/LandingPage.vue";
-import ArtistPage from "./components/ArtistPage.vue";
-import HomePage from "./components/HomePage.vue";
-import Footer from "./components/Footer.vue";
+import LandingPage from './components/LandingPage.vue';
+import ArtistPage from './components/ArtistPage.vue';
+import HomePage from './components/HomePage.vue';
+import Footer from './components/Footer.vue';
 
 export default {
-  components: {
-    ArtistPage,
-    // HomePage,
-    LandingPage,
-    Footer,
-  },
+   components: {
+      ArtistPage,
+      // HomePage,
+      LandingPage,
+      Footer,
+   },
 };
 </script>
 
 <template>
-  <LandingPage />
-  <ArtistPage />
-  <HomePage />
-  <!-- <Footer /> -->
+   <LandingPage />
+   <ArtistPage />
+   <HomePage />
+   <!-- <Footer /> -->
 </template>
 
 <style lang="scss">
-@import "./style/global.scss";
+@import './style/global.scss';
 </style>
