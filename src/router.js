339a03fd261fbce9ec88066351d6620e8f8b1ed9
dav100ide <oreglio.dev@gmail.com>
2023-02-21 12:14:16 +0100

    fix: bug404 -> NotFound

--- src/router.js
+++ src/router.js
@@ -4,5 +4,30 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './pages/HomePage.vue';
 import SearchResultsPage from './pages/SearchResultsPage.vue';
 import ArtistPage from './pages/ArtistPage';
+import NotFoundPage from './pages/NotFoundPage.vue';
 
-//
+const router = createRouter({
+   history: createWebHistory(),
+   //le rotte di tutte le pagine importate
+   routes: [
+      {
+         path: '/',
+         name: 'home-page',
+         component: HomePage,
+      },
+      {
+         // da rendere dinamico
+         path: '/artist',
+         name: 'artist-page',
+         component: ArtistPage,
+      },
+      {
+         // da rendere dinamico
+         path: '/:pathMatch(.*)*',
+         name: 'not-found-page',
+         component: ArtistPage,
+      },
+   ],
+});
+
+export { router };
