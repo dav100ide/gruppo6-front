f40ac91b425f981fab31904631bc5828a4d9094c
jiemarabanzado <jiemar.abanzado@yahoo.it>
2023-02-22 08:47:08 +0100

    aggiunta router al branch

---
+++ src/router.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+//import di tutte le pagine
+import HomePage from './pages/HomePage.vue';
+import SearchResultsPage from './pages/SearchResultsPage.vue';
+import ArtistPage from './pages/ArtistPage.vue';
+import NotFoundPage from './pages/NotFoundPage.vue';
+
+const router = createRouter({
+   history: createWebHistory(),
+   //le rotte di tutte le pagine importate
+   routes: [
+      {
+         path: '/',
+         name: 'home-page',
+         component: HomePage,
+      },
+      {
+         // path da rendere dinamico con la stringa digitata dal visitatore
+         path: '/search',
+         name: 'search-results-page',
+         component: SearchResultsPage,
+      },
+      {
+         // path dinamico con lo slug artista
+         path: '/artist/:slug',
+         name: 'artist-page',
+         component: ArtistPage,
+      },
+      {
+         path: '/:pathMatch(.*)*',
+         name: 'not-found-page',
+         component: NotFoundPage,
+      },
+   ],
+});
+
+export default router;
