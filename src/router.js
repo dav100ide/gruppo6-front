99b7caa32bbea151163b96bd2c3b2b93c4d49053
dav100ide <oreglio.dev@gmail.com>
2023-02-23 11:14:29 +0100

    fix rotta artist/slug-invalido

--- src/router.js
+++ src/router.js
@@ -5,10 +5,23 @@ import HomePage from './pages/HomePage.vue';
 import SearchResultsPage from './pages/SearchResultsPage.vue';
 import ArtistPage from './pages/ArtistPage.vue';
 import NotFoundPage from './pages/NotFoundPage.vue';
+import { store } from './store.js';
 
+/*===================================
+     funzioni per indirizzare le rotte
+=================================*/
+function checkIfArtistExists(slug) {
+   //prendo tutti gli artisti dello store
+   const artists = store.artists;
+   //ritorna un boolean, true se lo slugPARAMETRO è uguale allo slugARTISTA
+   return artists.some((artist) => artist.slug === slug);
+}
+
+/*==================================
+ le rotte di tutte le pagine importate
+ ====================================*/
 const router = createRouter({
    history: createWebHistory(),
-   //le rotte di tutte le pagine importate
    routes: [
       {
          path: '/',
@@ -27,12 +40,15 @@ const router = createRouter({
          name: 'artist-page',
          component: ArtistPage,
          beforeEnter: (to, from, next) => {
-            // qui effettuiamo una query al database per verificare se lo slug è valido
-            const isValidSlug = true; // true se lo slug è valido
-            if (isValidSlug) {
+            // Qui controlliamo se l'artista con lo slug specificato esiste o meno
+            const artistExists = checkIfArtistExists(to.params.slug);
+
+            if (artistExists) {
+               // Se l'artista esiste, possiamo proseguire alla visualizzazione della pagina dell'artista
                next();
             } else {
-               next({ name: 'not-found-page' }); // indirizziamo l'utente alla pagina 404
+               // Altrimenti, reindirizziamo l'utente alla pagina NotFound
+               next({ name: 'not-found-page' });
             }
          },
       },
