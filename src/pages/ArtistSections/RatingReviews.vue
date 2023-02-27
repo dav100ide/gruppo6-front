a83e96f8b53411e66386235de05bcf8e9064a084
dav100ide <oreglio.dev@gmail.com>
2023-02-27 17:25:56 +0100

    add cards review

--- src/pages/ArtistSections/RatingReviews.vue
+++ src/pages/ArtistSections/RatingReviews.vue
@@ -1,20 +1,23 @@
 <template>
    <section>
       <div class="ms-container p-5">
-         <h3 class="mb-3">Recensioni (statico + layout di base)</h3>
          <div class="stars mb-3">
             <div class="stars__text">Valutazione media:</div>
             <div class="stars__icons">
-               <i v-for="i in 3" class="fa-solid fa-star"></i>
-               <i v-for="i in 2" class="fa-regular fa-star"></i>
+               <i v-for="i in starsNumber" class="fa-solid fa-star"></i>
+               <i v-for="i in 5 - starsNumber" class="fa-regular fa-star"></i>
             </div>
          </div>
          <div class="container-fluid">
-            <div class="row">
+            <div class="row g-4">
                <!-- review col -->
-               <div v-for="review in artist.reviews" class="col-12 col-md-6 col-lg-4">
+               <div v-for="review in artist.reviews" class="col-12 col-md-6 col-xl-4">
                   <div class="review">
-                     {{ review.review_text }}
+                     <h4>{{ review.title }}</h4>
+                     <p>{{ review.review_text }}</p>
+                     <h5>
+                        {{ review.date }}
+                     </h5>
                   </div>
                </div>
                <!-- /review col -->
@@ -68,6 +71,18 @@ export default {
          },
       },
    },
+   computed: {
+      starsNumber() {
+         const ratings = this.artist.ratings;
+
+         let sum = 0;
+         ratings.forEach((rating) => {
+            sum += rating.rating;
+         });
+
+         return Math.round(sum / ratings.length);
+      },
+   },
 };
 </script>
 
@@ -87,11 +102,34 @@ section {
       flex-wrap: wrap;
       font-size: 1.5rem;
       gap: 0.5rem;
+      &__text {
+         text-shadow: 2px 2px 5px var(--neutral-color-100);
+      }
       &__icons {
          color: var(--accent-color);
          & i:not(:last-child) {
             margin-inline-end: 0.3rem;
          }
+      }
+   }
+
+   .review {
+      height: 100%;
+      padding: 2rem;
+      border-radius: 2.5rem;
+      background-color: var(--neutral-color-200);
+      color: var(--neutral-color-800);
+      display: flex;
+      flex-direction: column;
+      h4 {
+         font-size: 2rem;
+         font-weight: bold;
+         padding-bottom: 0.8rem;
+         border-bottom: 1px solid var(--neutral-color-600);
+      }
+      h5 {
+         margin-left: auto;
+         font-style: italic;
       }
    }
 }
