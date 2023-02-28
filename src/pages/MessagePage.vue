5ee6cf62af3bce9fba5ae18f2405cbdebf3602ab
dav100ide <oreglio.dev@gmail.com>
2023-02-28 21:45:28 +0100

    add photo cliccabile artista in MessagePage

--- src/pages/MessagePage.vue
+++ src/pages/MessagePage.vue
@@ -1,9 +1,21 @@
 <template>
    <section class="h-100">
       <div class="container-md px-3 py-5">
-         <h3 class="mb-4">
-            Scrivi un messaggio a <strong>{{ nickname }}</strong>
-         </h3>
+         <div class="top-bar mb-3">
+            <h3 class="pe-4">
+               Scrivi un messaggio a <strong>{{ nickname }}</strong>
+            </h3>
+            <RouterLink :to="{ name: 'artist-page', params: { slug: artistSlug } }">
+               <div class="photo">
+                  <img v-if="photo" :src="imgSrc" :alt="nickname" />
+                  <img
+                     v-else
+                     src="https://sgmh.org/wp-content/uploads/2017/07/placeholder-user.png"
+                     alt="placeholder"
+                  />
+               </div>
+            </RouterLink>
+         </div>
          <PostMessage />
       </div>
    </section>
@@ -37,14 +49,30 @@ export default {
             }
          });
    },
+
+   computed: {
+      imgSrc() {
+         return `http://127.0.0.1:8000/storage/${this.photo}`;
+      },
+   },
 };
 </script>
 
 <style lang="scss" scoped>
 h3 {
    color: var(--secondary-color);
+   margin: 0;
 }
 section {
    background-color: var(--neutral-color-600);
+}
+
+.top-bar {
+   display: flex;
+   align-items: center;
+   .photo {
+      @include circle-img(5.2rem);
+      box-shadow: 0 0 3px 3px var(--neutral-color-100);
+   }
 }
 </style>
