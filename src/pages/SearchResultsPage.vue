b333941d26570937848fde5fb000e75514270a36
jiemarabanzado <jiemar.abanzado@yahoo.it>
2023-02-28 01:13:26 +0100

    checkpoint

--- src/pages/SearchResultsPage.vue
+++ src/pages/SearchResultsPage.vue
@@ -11,24 +11,34 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
    data(){
       return{
+         sponsored:[],
+         all:[],
       }
    },
    created(){
+      let temp2=[];
+      let temp=[];
       axios.get('http://127.0.0.1:8000/api/artists') //
       .then((res) => {
+      this.all=res.data;
       temp=res.data;
       for (let index = 0; index < temp.length; index++) {
             let control=[]
             control=temp[index].sponsors;
             if (control[0]!=undefined) {
-               toShow.push(temp[index])
+               temp2.push(temp[index]);
             }
       }
-      this.artists=toShow;
+      console.log('temp2',temp2)
+      
    });
+   this.sponsored=temp2;
+    console.log('temp',this.sponsored)
+    console.log('all',this.all)
    }
 };
 </script>
