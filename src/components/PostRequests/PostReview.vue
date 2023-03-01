1b313115eb6949b483ccdacb55f5e320a46e1d2a
Stefano.In91 <ste.ince91@gmail.com>
2023-03-01 11:39:14 +0100

    removes required from postreview form

--- src/components/PostRequests/PostReview.vue
+++ src/components/PostRequests/PostReview.vue
@@ -1,45 +1,43 @@
 <template>
-   <div class="mb-3">
-      <label for="review_title">Titolo</label>
-      <input
-         class="form-control"
-         id="review_title"
-         type="text"
-         maxlength="30"
-         placeholder="Inserisci il titolo della Recensione"
-         required
-         v-model="store.review_title"
-         @keyup="$emit('change-title')"
-      />
-   </div>
-   <div class="bg-light">
-      <label for="review_text">Recensione</label>
-      <textarea
-         class="form-control"
-         id="review_text"
-         cols="30"
-         rows="10"
-         maxlength="1000"
-         placeholder="Inserisci Recensione"
-         required
-         v-model="store.review_text"
-         @keyup="$emit('change-text')"
-      >
-      </textarea>
-   </div>
+  <div class="mb-3">
+    <label for="review_title">Titolo</label>
+    <input
+      class="form-control"
+      id="review_title"
+      type="text"
+      maxlength="30"
+      placeholder="Inserisci il titolo della Recensione"
+      v-model="store.review_title"
+      @keyup="$emit('change-title')"
+    />
+  </div>
+  <div class="bg-light">
+    <label for="review_text">Recensione</label>
+    <textarea
+      class="form-control"
+      id="review_text"
+      cols="30"
+      rows="10"
+      maxlength="1000"
+      placeholder="Inserisci Recensione"
+      v-model="store.review_text"
+      @keyup="$emit('change-text')"
+    >
+    </textarea>
+  </div>
 </template>
 
 <script>
-import { store } from '../../store';
+import { store } from "../../store";
 
 export default {
-   name: 'PostReview',
+  name: "PostReview",
 
-   data() {
-      return {
-         store,
-      };
-   },
+  data() {
+    return {
+      store,
+    };
+  },
 };
 </script>
 
