ecb8022f086adbf7d7fb2b3bf75226a15b864b85
Stefano.In91 <ste.ince91@gmail.com>
2023-03-02 11:35:54 +0100

    push arrogante adds conditions to review posting

--- src/components/PostRequests/PostReview.vue
+++ src/components/PostRequests/PostReview.vue
@@ -1,69 +1,69 @@
 <template>
-   <div class="container-fluid">
-      <div class="row g-3 mb-3">
-         <div class="col-12 col-md-7 col-lg-5">
-            <label for="review_title">Titolo</label>
-            <input
-               class="form-control"
-               id="review_title"
-               type="text"
-               maxlength="30"
-               placeholder="Inserisci il titolo della Recensione"
-               v-model="store.review_title"
-               @keyup="$emit('change-title')"
-            />
-         </div>
-
-         <div class="col-12">
-            <label for="review_text">Recensione</label>
-            <textarea
-               class="form-control"
-               id="review_text"
-               cols="30"
-               rows="10"
-               maxlength="1000"
-               placeholder="Inserisci Recensione"
-               v-model="store.review_text"
-               @keyup="$emit('change-text')"
-            >
-            </textarea>
-         </div>
+  <div class="container-fluid">
+    <div class="row g-3 mb-3">
+      <div class="col-12 col-md-7 col-lg-5">
+        <label for="review_title">Titolo</label>
+        <input
+          class="form-control"
+          id="review_title"
+          type="text"
+          maxlength="30"
+          placeholder="Inserisci il titolo della Recensione"
+          v-model="store.review_title"
+          @keyup="$emit('change-title')"
+        />
       </div>
-   </div>
+
+      <div class="col-12">
+        <label for="review_text">Recensione</label>
+        <textarea
+          class="form-control"
+          id="review_text"
+          cols="30"
+          rows="10"
+          maxlength="1000"
+          placeholder="Inserisci Recensione"
+          v-model="store.review_text"
+          @keyup="$emit('change-text')"
+        >
+        </textarea>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { store } from '../../store';
+import { store } from "../../store";
 
 export default {
-   name: 'PostReview',
+  name: "PostReview",
 
-   data() {
-      return {
-         store,
-      };
-   },
+  data() {
+    return {
+      store,
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 :is(input, textarea):focus {
-   box-shadow: 2px 2px var(--accent-color) !important;
-   //important pk sovrascrivo bootstrap
+  box-shadow: 2px 2px var(--accent-color) !important;
+  //important pk sovrascrivo bootstrap
 }
 
 label {
-   display: none;
+  display: none;
 }
 
 .form-control {
-   padding: 1.3rem;
-   border-radius: 1.5625rem;
-   font-size: 1.2rem;
-   color: var(--neutral-color-100);
-   &::placeholder {
-      color: var(--neutral-color-200);
-      font-style: italic;
-   }
+  padding: 1.3rem;
+  border-radius: 1.5625rem;
+  font-size: 1.2rem;
+  color: var(--neutral-color-100);
+  &::placeholder {
+    color: var(--neutral-color-200);
+    font-style: italic;
+  }
 }
 </style>
--- src/pages/RatingReviewPage.vue
+++ src/pages/RatingReviewPage.vue
@@ -1,140 +1,148 @@
 <template>
-   <section>
-      <div class="container-md px-3 py-5">
-         <div class="top-bar mb-3">
-            <h3>
-               Lascia una Recensione a <strong>{{ artistName }}</strong>
-            </h3>
-            <PostRating @change-rating="changeRating()" />
-         </div>
-         <form @submit.prevent="postAll()">
-            <PostReview @change-title="changeTitle()" @change-text="changeText()" />
-            <button type="submit" class="button-28">Invia</button>
-         </form>
+  <section>
+    <div class="container-md px-3 py-5">
+      <div class="top-bar mb-3">
+        <div v-if="missing_data" class="alert alert-danger">Inserisci tutti i dati</div>
+        <h3>
+          Lascia una Recensione a <strong>{{ artistName }}</strong>
+        </h3>
+        <PostRating @change-rating="changeRating()" />
       </div>
-   </section>
+      <form @submit.prevent="postAll()">
+        <PostReview @change-title="changeTitle()" @change-text="changeText()" />
+        <button type="submit" class="button-28">Invia</button>
+      </form>
+    </div>
+  </section>
 </template>
 
 <script>
-import axios from 'axios';
-import PostRating from '../components/PostRequests/PostRating.vue';
-import PostReview from '../components/PostRequests/PostReview.vue';
-import { store } from '../store';
+import axios from "axios";
+import PostRating from "../components/PostRequests/PostRating.vue";
+import PostReview from "../components/PostRequests/PostReview.vue";
+import { store } from "../store";
 
 export default {
-   name: 'RatingReviewPage',
-   components: {
-      PostRating,
-      PostReview,
-   },
+  name: "RatingReviewPage",
+  components: {
+    PostRating,
+    PostReview,
+  },
 
-   data() {
-      return {
-         store,
-         formData: {
-            rating_id: 0,
-            artist_id: 0,
-            title: '',
-            review_text: '',
-         },
-      };
-   },
-   methods: {
-      changeRating() {
-         this.formData.rating_id = this.store.rating_id;
+  data() {
+    return {
+      store,
+      formData: {
+        rating_id: 0,
+        artist_id: 0,
+        title: "",
+        review_text: "",
       },
-      changeTitle() {
-         this.formData.title = this.store.review_title;
-      },
-      changeText() {
-         this.formData.review_text = this.store.review_text;
-      },
-      postAll() {
-         console.log(this.formData.artist_id);
-         // se c'è, posto la review
-         if (this.formData.title != '' && this.formData.review_text != '') {
-            axios
-               .post('http://127.0.0.1:8000/api/send-review', {
-                  artist_id: this.formData.artist_id,
-                  title: this.formData.title,
-                  review_text: this.formData.review_text,
-               })
-               .then((res) => {
-                  console.log(res.data, 'review sent');
-               })
-               .catch((error) => {
-                  console.error(error.response.data);
-               });
-         }
-         // resetto variabili utilizzate nello store e formData
-         this.store.review_title = '';
-         this.changeTitle();
-         this.store.review_text = '';
-         this.changeText();
+      missing_data: false,
+    };
+  },
+  methods: {
+    changeRating() {
+      this.formData.rating_id = this.store.rating_id;
+    },
+    changeTitle() {
+      this.formData.title = this.store.review_title;
+    },
+    changeText() {
+      this.formData.review_text = this.store.review_text;
+    },
+    postAll() {
+      console.log(this.formData.artist_id);
+      // se c'è, posto la review
+      if (this.formData.title != "" && this.formData.review_text != "") {
+        axios
+          .post("http://127.0.0.1:8000/api/send-review", {
+            artist_id: this.formData.artist_id,
+            title: this.formData.title,
+            review_text: this.formData.review_text,
+          })
+          .then((res) => {
+            console.log(res.data, "review sent");
+          })
+          .catch((error) => {
+            console.error(error.response.data);
+          });
+        // resetto variabili utilizzate nello store e formData
+        this.store.review_title = "";
+        this.changeTitle();
+        this.store.review_text = "";
+        this.changeText();
+      } else if (
+        (this.formData.title != "" && this.formData.review_text === "") ||
+        (this.formData.title === "" && this.formData.review_text != "")
+      ) {
+        // questo controllo mi deve dire che il dato mancante è richiesto
+        return (this.missing_data = true);
+      }
 
-         // se è stato selezionato un rating, lo posto
-         if (this.formData.rating_id != '') {
-            axios
-               .post('http://127.0.0.1:8000/api/send-rating', {
-                  artist_id: this.formData.artist_id,
-                  rating_id: this.formData.rating_id,
-               })
-               .then((res) => {
-                  console.log(res.data, 'rating sent');
-               })
-               .catch((error) => {
-                  console.error(error.response.data);
-               });
-            //resetto variabili utilizzate nello store e formData
-            this.store.rating_id = 0;
-            this.changeRating();
-         }
-         //redirect ad artist page di partenza
-         this.$router.push({ name: 'artist-page', params: { slug: this.$route.params.slug } });
-         console.log('redirected from ratingreview page');
-      },
-   },
-   created() {
-      axios
-         .get(`http://127.0.0.1:8000/api/artist-id/${this.$route.params.slug}`) //
-         .then((res) => {
-            this.formData.artist_id = res.data;
-            console.log(res.data);
-         })
-         .catch((err) => {
-            if (err.response.status === 404) {
-               this.$router.push({ name: 'not-found-page' });
-            }
-         });
-   },
+      // se è stato selezionato un rating, lo posto
+      if (this.formData.rating_id != "") {
+        axios
+          .post("http://127.0.0.1:8000/api/send-rating", {
+            artist_id: this.formData.artist_id,
+            rating_id: this.formData.rating_id,
+          })
+          .then((res) => {
+            console.log(res.data, "rating sent");
+          })
+          .catch((error) => {
+            console.error(error.response.data);
+          });
+        //resetto variabili utilizzate nello store e formData
+        this.store.rating_id = 0;
+        this.changeRating();
+      }
+      //redirect ad artist page di partenza
+      this.$router.push({ name: "artist-page", params: { slug: this.$route.params.slug } });
+      console.log("redirected from ratingreview page");
+    },
+  },
+  created() {
+    axios
+      .get(`http://127.0.0.1:8000/api/artist-id/${this.$route.params.slug}`) //
+      .then((res) => {
+        this.formData.artist_id = res.data;
+        console.log(res.data);
+      })
+      .catch((err) => {
+        if (err.response.status === 404) {
+          this.$router.push({ name: "not-found-page" });
+        }
+      });
+  },
 
-   computed: {
-      artistName() {
-         //prendo il nome artista senza la chiamata axios, prendo lo slug dall'url e lo trasformo
-         const slug = this.$route.params.slug;
-         const words = slug.split('-');
-         return words.join(' ');
-      },
-   },
+  computed: {
+    artistName() {
+      //prendo il nome artista senza la chiamata axios, prendo lo slug dall'url e lo trasformo
+      const slug = this.$route.params.slug;
+      const words = slug.split("-");
+      return words.join(" ");
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 section {
-   background-color: var(--neutral-color-600);
-   h3 {
-      color: var(--secondary-color);
-      margin: 0;
-      strong {
-         text-transform: capitalize;
-      }
-   }
+  background-color: var(--neutral-color-600);
+  h3 {
+    color: var(--secondary-color);
+    margin: 0;
+    strong {
+      text-transform: capitalize;
+    }
+  }
 
-   .top-bar {
-      display: flex;
-      gap: 2.5rem;
-      flex-wrap: wrap;
-      align-items: center;
-   }
+  .top-bar {
+    display: flex;
+    gap: 2.5rem;
+    flex-wrap: wrap;
+    align-items: center;
+  }
 }
 </style>
