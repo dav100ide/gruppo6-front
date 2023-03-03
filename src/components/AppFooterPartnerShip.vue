09f0bcaae724c3f4d220f4787be5603053616dc0
Alex100688 <alessiozingaro2@gmail.com>
2023-03-03 03:46:42 +0100

    Aggiunge AppFooterPartnerShip

---
+++ src/components/AppFooterPartnerShip.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "AppFooterPartnerShip",
+};
+</script>
+<template>
+  <section><h6>Qui inseriamo tutti i partnerships</h6></section>
+</template>
+<style lang="scss" scoped>
+h6 {
+  color: white;
+}
+</style>
