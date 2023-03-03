e1e4604accc33797b0df61f66878bde216b9f1d0
Alex100688 <alessiozingaro2@gmail.com>
2023-03-03 03:41:29 +0100

    Aggiune AppFooterInformazioni

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,26 @@
 <script>
 import AppFooterTop from "./AppFooterTop.vue";
+import AppFooterBottom from "./AppFooterBottom.vue";
 export default {
   name: "AppFooter",
   components: {
     AppFooterTop,
+    AppFooterBottom,
   },
 };
 </script>
+<template>
+  <div class="container">
+    <!-- dove c'è scritto Logo ci vuole il logo -->
+    <p class="text-center logo mb-5">Logo</p>
+    <AppFooterTop />
+    <AppFooterBottom class="mt-5" />
+  </div>
+</template>
 
-<template></template>
-
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h3,
+p {
+  color: white;
+}
+</style>
---
+++ src/components/AppFooterInformazioni.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "AppFooterInformazioni",
+};
+</script>
+<template>
+  <section>
+    <small>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit
+      deleniti delectus eum consequuntur, rem neque molestiae, nemo debitis hic
+      iste quaerat incidunt quae voluptate quibusdam laboriosam vero odio
+      libero. Perferendis?
+    </small>
+  </section>
+</template>
+<style lang="scss" scoped>
+small {
+  color: #e5e5e5;
+}
+</style>
