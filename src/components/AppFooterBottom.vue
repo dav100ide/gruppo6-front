d1467a5f4f18199b3866abcb88b9e308fde3d120
Alex100688 <alessiozingaro2@gmail.com>
2023-03-03 03:39:57 +0100

    Aggiunge footertop e footerbottom

---
+++ src/components/AppFooterBottom.vue
@@ -0,0 +1,23 @@
+<script>
+import AppFooterSocial from "./AppFooterSocial.vue";
+import AppFooterCopyright from "./AppFooterCopyright.vue";
+export default {
+  name: "AppFooterBottom",
+  components: {
+    AppFooterSocial,
+    AppFooterCopyright,
+  },
+};
+</script>
+<template>
+  <section class="container mt-5">
+    <AppFooterSocial />
+    <hr />
+    <AppFooterCopyright />
+  </section>
+</template>
+<style lang="scss" scoped>
+hr {
+  color: white;
+}
+</style>
--- src/components/AppFooterTop.vue
+++ src/components/AppFooterTop.vue
@@ -1,5 +1,42 @@
 <script>
+import AppFooterInformazioni from "./AppFooterInformazioni.vue";
+import AppFooterContatti from "./AppFooterContatti.vue";
+import AppFooterPartnerShip from "./AppFooterPartnerShip.vue";
+import AppFooterPrivacy from "./AppFooterPrivacy.vue";
 export default {
   name: "AppFooterTop",
+  components: {
+    AppFooterInformazioni,
+    AppFooterContatti,
+    AppFooterPartnerShip,
+    AppFooterPrivacy,
+  },
 };
 </script>
+<template>
+  <section class="container">
+    <div class="row">
+      <div class="col-3">
+        <h5>Su di noi</h5>
+        <AppFooterInformazioni />
+      </div>
+      <div class="col-3">
+        <h5 class="ms-5">Contattaci</h5>
+        <AppFooterContatti class="ms-3" />
+      </div>
+      <div class="col-3">
+        <h5>I nostri partnership</h5>
+        <AppFooterPartnerShip />
+      </div>
+      <div class="col-3">
+        <h5 class="ms-5">Privacy e Sicurezza</h5>
+        <AppFooterPrivacy class="ms-3" />
+      </div>
+    </div>
+  </section>
+</template>
+<style lang="scss" scoped>
+h5 {
+  color: white;
+}
+</style>
