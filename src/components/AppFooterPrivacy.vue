943fdc6dd36b4d71cc088623c56d7569cf99c93b
Alex100688 <alessiozingaro2@gmail.com>
2023-03-03 03:51:15 +0100

    Aggiunge AppFooterPrivacy

---
+++ src/components/AppFooterPrivacy.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "AppFooterPrivacy",
+};
+</script>
+<template>
+  <section>
+    <ul>
+      <li><small>Privacy Policy</small></li>
+      <li><small>Social Media Policy</small></li>
+      <li><small>Copyright Notice</small></li>
+    </ul>
+  </section>
+</template>
+<style lang="scss" scoped>
+ul {
+  list-style: none;
+  color: #e5e5e5;
+}
+</style>
