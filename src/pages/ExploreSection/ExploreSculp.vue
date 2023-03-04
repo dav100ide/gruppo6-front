7687d53256fe8f4c0a68475584a8ef464e5037d9
jiemarabanzado <jiemar.abanzado@yahoo.it>
2023-03-04 10:31:21 +0100

    scultore e fotografi responsive

--- src/pages/ExploreSection/ExploreSculp.vue
+++ src/pages/ExploreSection/ExploreSculp.vue
@@ -4,9 +4,10 @@
     export default{
         data(){
             return{
-                Sculptors:[],
+                Directors:[],
                 minRating:0,
-                minReviews:0
+                minReviews:0,
+                activeMobile: 0,
             }
         },
         methods:{
@@ -54,16 +55,29 @@
                             controllati.push(filtrati[index]);
                         }
                     }
-                    this.Sculptors=controllati;
+                    this.Directors=controllati;
                 });         
-            }
+            },
+            forward() {
+                if (this.activeMobile == this.Directors.length - 1) {
+                    this.activeMobile = 0;
+                } else {
+                    this.activeMobile += 1;
+                }
+            },
+            back() {
+                if (this.activeMobile == 0) {
+                    this.activeMobile = this.Directors.length - 1;
+                } else {
+                this.activeMobile -= 1;
+                }
+            },
         },
         created(){
             let temp=[];
             let FilteredArtist=[]
             axios.get('http://127.0.0.1:8000/api/artists').then((res)=>{
                 temp=res.data;
-                console.log('temp hprto',temp);
                 for (let index = 0; index < temp.length; index++) {
                     let ItIS=false;
                     const element = temp[index].techniques;
@@ -77,7 +91,7 @@
                         FilteredArtist.push(temp[index])
                     }
                 }
-                this.Sculptors=FilteredArtist;
+                this.Directors=FilteredArtist;
             });
         }
 
@@ -87,20 +101,21 @@
     <div class="filters-row">
         <h2>Scultori</h2>
         <div class="filters">
-            <small>numero recensioni: </small>  
+            <small>n. recensioni: </small>  
             <input type="number" min="0" id="reviews-num">
-            <small>valutazione: </small>   
+            <small>voto: </small>   
             <input type="number" min="0" max="5" id="rating">
             <button id="filter" @click="Filter">filtra</button>
         </div>
     </div>
-    <div class="all-section">
-        <h2 v-if="Sculptors.length==0">non ci sono artisti che soddisfano i parametri !!</h2>
-        <div class="single-card" v-for="(artist , index) in Sculptors">
-            <RouterLink :to="{ name: 'artist-page', params: { slug: artist.slug } }">
-                <div class="ex-card-img">
+    <h2 v-if="Directors.length==0">non ci sono artisti che soddisfano i parametri !!</h2>
+    <div class="d-none d-sm-block">
+        <div class="ms-grid">
+            <div class="ms-card" v-for="(artist , index) in Directors">
+                <RouterLink :to="{ name: 'artist-page', params: { slug: artist.slug } }">
+                    <div class="card-img">
                     <!-- foto caricata dall'utente -->
-                    <img
+                        <img
                            v-if="artist.profile_photo"
                            :src="artist.profile_photo"
                            :alt="artist.artist_nickname"
@@ -120,17 +135,66 @@
                            alt="placeholder"
                         />
                         <!-- /fallback foto se l'utente non carica -->
-                </div>
-                <div class="card-data">
-                    <h5 class="ex-card-name">
-                        {{artist.artist_nickname }}
-                    </h5>
-                    <div class="tecnique" v-for="tecnique in artist.techniques">
-                        <small v-if="tecnique.name=='Scultore'">{{ tecnique.name }}</small>
                     </div>
-                </div>
-            </RouterLink>
+                    <div class="card-data">
+                        <h5 class="py-2 fw-bold">
+                            {{artist.artist_nickname }}
+                        </h5>
+                        <div class="tecnique" v-for="tecnique in artist.techniques">
+                            <small v-if="tecnique.name=='Scultore'">{{ tecnique.name }}</small>
+                        </div>
+                    </div>
+                </RouterLink>
+            </div>
         </div>
+    </div>
+    <div class="sponsored-mobile d-block d-sm-none" v-if="Directors.length!=0">
+        <!-- carosello mobile  -->
+        <div class="mobile-carousel">
+            <div
+                class="mobile-card"
+                v-for="(artist, index) in Directors"
+                :class="this.activeMobile == index ? 'show-mobile' : ''"
+            >
+                <RouterLink :to="{ name: 'artist-page', params: { slug: artist.slug } }">
+                    <div class="positioning">
+                        <div class="mobile-card-img">
+                           <!-- foto caricata dall'utente -->
+                           <img
+                              v-if="artist.profile_photo"
+                              :src="artist.profile_photo"
+                              :alt="artist.artist_nickname"
+                           />
+                           <!-- /foto caricata dall'utente -->
+                           <!-- foto seedata -->
+                           <img
+                              v-else-if="artist.seeded_pic"
+                              :src="artist.seeded_pic"
+                              :alt="artist.artist_nickname"
+                           />
+                           <!-- /foto seedata -->
+                           <!-- fallback foto se l'utente non carica -->
+                           <img
+                              v-else
+                              src="https://www.sanitascare.it/wp-content/uploads/2017/04/default-user-image.png"
+                              alt="placeholder"
+                           />
+                           <!-- /fallback foto se l'utente non carica -->
+                        </div>
+                        <div class="mobile-card-description">
+                            <h4>{{ artist.artist_nickname }}</h4>
+                        </div>
+                    </div>
+                </RouterLink>
+            </div>
+            <button @click="forward" id="forward">
+                <i class="fa-solid fa-chevron-right"></i>
+            </button>
+            <button @click="back" id="back">
+                <i class="fa-solid fa-chevron-left"></i>
+            </button>
+        </div>
+            <!-- carosello mobile -->
     </div>
     
 
@@ -142,6 +206,11 @@
         align-items: center;
         justify-content: space-between;
         margin-bottom: 20px;
+        @media (width < 567px) {
+            flex-direction: column;
+            justify-content: flex-start;
+            align-items: start;
+        }
         
     }
     #reviews-num{
@@ -155,7 +224,7 @@
     #rating{
         background-color: transparent;
         border: none;
-        color: white;
+        color:white;
         margin:0px 15px;
         border-bottom: 1px solid white;
     }
