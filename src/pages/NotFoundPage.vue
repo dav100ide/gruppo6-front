96dfdedab7fef61dd683d61663cdcc2a81d6c5df
dav100ide <oreglio.dev@gmail.com>
2023-03-05 18:14:16 +0100

    404 lol in 2secondi

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -1,11 +1,19 @@
 <template>
-   <div>
-      <h1 class="fw-bold">404: non si trova la pagina</h1>
-   </div>
+   <section>
+      <h1 class="fw-bold"><strong>404</strong>: non si trova la pagina</h1>
+   </section>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+section {
+   height: calc(100vh - var(--header-height));
+   @include flex-center;
+   strong {
+      color: var(--primary-color-200);
+   }
+}
+</style>
